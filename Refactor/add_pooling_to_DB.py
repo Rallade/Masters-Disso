91d3a07dba2c61e125a788c84207a547011c2c1f
Rallade <rallade@hotmail.com>
2019-08-08 22:55:42 +0200

    DB now includes title and embedding

--- Refactor/add_pooling_to_DB.py
+++ Refactor/add_pooling_to_DB.py
@@ -7,7 +7,7 @@ from db_upload import attach_listfields_to_records
 
 client = MongoClient()
 db = client.disso
-coll = db.cotswaldsdata_copy
+coll = db.cotswaldsdata
 
 def paginate(page_size, page_num):
     skips = page_size * page_num
@@ -17,12 +17,13 @@ def paginate(page_size, page_num):
     return [x for x in cursor]
 
 
-batch_size = 3000
+batch_size = 1500
 i = 0
 t0 = time.time()
 while True:
     records = paginate(batch_size, i)
     i += 1
+    
     embeddings = []
     for record in records:
         try:
@@ -32,6 +33,7 @@ while True:
     pro_means = list(pool.map(pooling.reduce_mean,embeddings))
     pro_maxes_s = list(pool.map(pooling.reduce_max_single,embeddings))
     pro_maxes_t = list(pool.map(pooling.reduce_max_total,embeddings))
+
     embeddings = []
     for record in records:
         try:
@@ -41,12 +43,30 @@ while True:
     con_means = list(pool.map(pooling.reduce_mean,embeddings))
     con_maxes_s = list(pool.map(pooling.reduce_max_single,embeddings))
     con_maxes_t = list(pool.map(pooling.reduce_max_total,embeddings))
+
+    embeddings = []
+    for record in records:
+        try:
+            embeddings.append(record['full_title_embedding'])
+        except:
+            embeddings.append(None)
+    title_means = list(pool.map(pooling.reduce_mean,embeddings))
+    title_maxes_s = list(pool.map(pooling.reduce_max_single,embeddings))
+    title_maxes_t = list(pool.map(pooling.reduce_max_total,embeddings))
+
+
     attach_listfields_to_records("mean_pooling_pros", pro_means, records, coll)
     attach_listfields_to_records("max_pooling_single_pros", pro_maxes_s, records, coll)
     attach_listfields_to_records("max_pooling_total_pros", pro_maxes_t, records, coll)
     attach_listfields_to_records("mean_pooling_cons", con_means, records, coll)
     attach_listfields_to_records("max_pooling_single_cons", con_maxes_s, records, coll)
     attach_listfields_to_records("max_pooling_total_cons", con_maxes_t, records, coll)
+    attach_listfields_to_records("mean_pooling_title", title_means, records, coll)
+    attach_listfields_to_records("max_pooling_single_title", title_maxes_s, records, coll)
+    attach_listfields_to_records("max_pooling_total_title", title_maxes_t, records, coll)
+
+    print("Records done:", i * batch_size)
+
     if not records:
         break
 t1 = time.time()
--- Refactor/create_DB.py
+++ Refactor/create_DB.py
@@ -84,3 +84,28 @@ for i, record in enumerate(full):
             db_upload.attach_listfields_to_records("cons_tokens", tokens, batch, coll)
             print("Percentage till completion:", str(i*100/len(full)) + "%")
         batch = []
+
+print("Starting titles")
+
+full = sorted(full, key=lambda x: len(x['Product title']))
+for i, record in enumerate(full):
+    batch.append(record)
+    if i % (number_of_processors*batch_size) == (number_of_processors*batch_size)-1:
+        curr_batch = list(map(lambda x: x['Product title'], batch))
+        # remove all empty strings
+        try:
+            idx_of_last_empty = len(curr_batch) - curr_batch[::-1].index("") - 1
+        except:
+            idx_of_last_empty = -1
+        curr_batch = curr_batch[idx_of_last_empty + 1:]
+        if curr_batch:
+            server_return = bc.encode(curr_batch, show_tokens=True)
+            cons_embeddings = server_return[0]
+            tokens = server_return[1]
+            cons_embeddings = list(map(lambda x,y: x[:len(y)], cons_embeddings, tokens))
+            batch = batch[idx_of_last_empty + 1:]
+            assert len(cons_embeddings) == len(batch) == len(tokens)
+            db_upload.attach_listfields_to_records("full_title_embedding", cons_embeddings, batch, coll)
+            db_upload.attach_listfields_to_records("title_tokens", tokens, batch, coll)
+            print("Percentage till completion:", str(i*100/len(full)) + "%")
+        batch = []
